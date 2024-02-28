adc801c48ca991d3fbb2a6b0bd692a9cd3f2f785
Claudia <claudiatcruz2002@gmail.com>
2024-02-28 20:50:26 +0100

    Database connection verified

---
+++ node/app.js
@@ -0,0 +1,28 @@
+/**
+ *
+ * NOMBRE: app.js
+ * AUTORA: Claudia Torres Cruz
+ * FECHA: 28/02/2024
+ * DESCRIPCIÓN: En este fichero se encuentra la lógica verdadera encargada de recoger datos de la base de datos
+ *
+ */
+import express from 'express';
+import cors from 'cors'
+// conection db
+import db from './database/db.js'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+try {
+    await db.authenticate()
+    console.log('Conexion exitoa a la base de datos')
+} catch (error) {
+    console.log(`El error de conexion es: ${error}`)
+}
+
+app.listen(8000, ()=>{
+    console.log('Server UP running in http://localhost:8000/')
+})
