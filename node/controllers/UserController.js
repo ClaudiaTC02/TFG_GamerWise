c9e7874d9d7aefb78d1de7e6a805eba3e8a82eee
Claudia <claudiatcruz2002@gmail.com>
2024-03-22 18:58:50 +0100

    refactor - Move code to new folder

--- node/controllers/UserController.js
+++ node/controllers/UserController.js
@@ -2,8 +2,8 @@
 import UserModel from "../models/UserModel.js";
 // security
 import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken';
-const saltRounds = 10;
+//import modular functions
+import { validateRequiredFields, validateEmail, validateDataTypes, validatePassword, hashPassword, generateAuthToken } from '../utils/userUtils.js';
 //----------------------------------------------------------------------
 // CRUD Methods
 //----------------------------------------------------------------------
@@ -56,7 +56,6 @@ export const login = async (req, res) =>{
         if (!validateEmail(email)) {
             return res.status(400).json({ message: "Invalid email format"})
         }
-        let hashedPassword = await hashPassword(password);
         let user = await UserModel.findOne({
             where: { email: email}
         })
@@ -67,68 +66,8 @@ export const login = async (req, res) =>{
         const token = generateAuthToken(user.id);
         res.status(200).json({user, token})
     } catch (error) {
+        console.log(error.message);
         // Internal server error
         res.status(500).json({ message: error.message });
     }
 }
-
-//----------------------------------------------------------------------
-// Modular functions
-//----------------------------------------------------------------------
-const validateRequiredFields = (fields, requiredFields) => {
-    for (let field of requiredFields) {
-        if (!fields[field]) {
-            return false;
-        }
-    }
-    return true
-}
-
-const validateEmail = (email) =>{
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email)
-}
-
-const validateDataTypes = (fields) => {
-    const dataTypes = {
-        email: "string",
-        name: "string",
-        password: "string"
-    };
-    for (let key in fields) {
-        if (typeof fields[key] !== dataTypes[key]) {
-        return false;
-        }
-    }
-    return true;
-};
-
-const validatePassword = (password) => {
-    // Verify length
-    if (password.length < 8) {
-      //throw new Error("Password must be at least 8 characters long");
-        return false
-    }
-    // Verify symbols, uppercase and lowercase
-    const uppercaseRegex = /[A-Z]/;
-    const lowercaseRegex = /[a-z]/;
-    const symbolRegex = /[$&+,:;=?@#|'<>.^*()%!-]/;
-    if (!uppercaseRegex.test(password) || !lowercaseRegex.test(password) || !symbolRegex.test(password)) {
-        //throw new Error("Password must contain at least one uppercase letter, one lowercase letter, and one symbol");
-        return false
-    }
-    return true
-};
-
-const hashPassword = async (password) => {
-    try {
-        const hash = await bcrypt.hash(password, saltRounds);
-        return hash;
-    } catch (error) {
-        throw error; // Rechazar la promesa en caso de error
-    }
-};
-
-const generateAuthToken = (userId) => {
-    return jwt.sign({ userId }, 'eriwekhfoiqw', { expiresIn: '1h' });
-};
