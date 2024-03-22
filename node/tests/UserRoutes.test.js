55368df600f8f1a732cd9f3987937a6292823eed
Claudia <claudiatcruz2002@gmail.com>
2024-03-22 18:59:06 +0100

    test - login test

--- node/tests/UserRoutes.test.js
+++ node/tests/UserRoutes.test.js
@@ -1,10 +1,12 @@
-import testServer from './testServer.js'
+import testServer from '../utils/testServer.js'
 import userRoutes from '../routes/UserRoutes.js'
 import UserModel from '../models/UserModel.js';
+import {hashPassword} from '../utils/userUtils.js'
 
 const request = testServer(userRoutes)
 
 describe('[ routes / user ]', () => {
+
     it('should create a new user', async () => {
         // Arrage
         const user = { email: 'test@test.com', name: 'test', password: 'Test12345678!'}
@@ -58,9 +60,9 @@ describe('[ routes / user ]', () => {
 
     it('should not create a new user with existing email', async () => {
         // Arrage
-        const user = { email: 'test@test.com', name: 'test', password: 'Test12345678!'}
+        const user1 = { email: 'test@test.com', name: 'test', password: 'Test12345678!'}
         const user2 = { email: 'test@test.com', name: 'user', password: 'Test12345678!'}
-        await request.post('/user').send(user)
+        await UserModel.create(user1)
         // Act
         const {status, body} = await request.post('/user').send(user2)
         // Assert
@@ -68,8 +70,94 @@ describe('[ routes / user ]', () => {
         expect(body.message).toEqual('Email already exists')
     });
     
+    it('should not create a new user with empty name', async () => {
+        // Arrage
+        const userEmptyName = { email: 'test@test.com', name: '', password: 'Test12345678!'}
+        // Act
+        const {status, body} = await request.post('/user').send(userEmptyName)
+        // Assert
+        expect(status).toEqual(400)
+        expect(body.message).toEqual('Required fields')
+    });
+
     // destroy all items
     afterEach(async () =>{
         await UserModel.destroy({where: {}})
     })
+
 });
+
+describe('[routes / user/ login ]', () =>{
+
+    beforeAll(async () =>{
+        const hashedPassword = await hashPassword("Password123?")
+        await UserModel.create({
+            name: "Claudia",
+            password: hashedPassword,
+            email: "claudia@gmail.com"
+        })
+    })
+    it('should login a user with valid credentials', async () => {
+        // Arrage
+        const l_validUser = {email: 'claudia@gmail.com', password:'Password123?'}
+        // Act
+        const {status, body} = await request.post('/user/login').send(l_validUser)
+        // Assert
+        expect(status).toEqual(200)
+        expect(body.token).toBeDefined()
+    });
+
+    it('should NOT login a user with invalid email', async () => {
+        // Arrage
+        const l_Invaliduser = {email: 'tester@gmail.com', password:'Password123?'}
+        // Act
+        const {status, body} = await request.post('/user/login').send(l_Invaliduser)
+        // Assert
+        expect(status).toEqual(404)
+        expect(body.message).toEqual('User not found or incorrect password')
+    });
+
+    it('should NOT login a user with invalid password', async () => {
+        // Arrage
+        const l_user = {email: 'claudia@gmail.com', password:'Password3?'}
+        // Act
+        const {status, body} = await request.post('/user/login').send(l_user)
+        // Assert
+        expect(status).toEqual(404)
+        expect(body.message).toEqual('User not found or incorrect password')
+    });
+
+    it('should NOT login a user with incompleted fields', async () => {
+        // Arrage
+        const l_Incompleteuser = {password:'Password3?'}
+        // Act
+        const {status, body} = await request.post('/user/login').send(l_Incompleteuser)
+        // Assert
+        expect(status).toEqual(400)
+        expect(body.message).toEqual('Required fields')
+    });
+
+    it('should NOT login a user with invalid data types', async () => {
+        // Arrage
+        const l_InvalidDatauser = {email: 'claudia@gmail.com', password: 123}
+        // Act
+        const {status, body} = await request.post('/user/login').send(l_InvalidDatauser)
+        // Assert
+        expect(status).toEqual(400)
+        expect(body.message).toEqual('Invalid data type')
+    });
+
+    it('should NOT login a user with invalid email format', async () => {
+        // Arrage
+        const l_InvalidEmailuser = {email: '@gmail.com', password: 'Password123?'}
+        // Act
+        const {status, body} = await request.post('/user/login').send(l_InvalidEmailuser)
+        // Assert
+        expect(status).toEqual(400)
+        expect(body.message).toEqual('Invalid email format')
+    });
+
+    afterAll(async () =>{
+        await UserModel.destroy({where: {}})
+    })
+})
