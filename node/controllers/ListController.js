fdcb5138243a389452fb8bee4d6c9804f0eaedc3
Claudia <claudiatcruz2002@gmail.com>
2024-03-30 18:31:51 +0100

    refactor - List controller and logic separated

--- node/controllers/ListController.js
+++ node/controllers/ListController.js
@@ -1,149 +1,83 @@
-//import Model
-import ListModel from "../models/ListModel.js";
-import UserModel from "../models/UserModel.js";
-import ListGameModel from "../models/ListGameModel.js";
+// import Logic
+import { createListLogic, getAllListLogic,updateListLogic,deleteListLogic  } from '../logic/ListLogic.js';
 
 //----------------------------------------------------------------------
-// CRUD Methods
+// HTTP
 //----------------------------------------------------------------------
 
-// create a new list
-export const createList = async (req, res) =>{
+// create a list
+export const createList = async (req, res) => {
     try {
-        let {name, description, user_id} = req.body;
-        // validate if it has name and user_id
-        if(!name || !user_id){
-            return res.status(400).json({message:"Name and user_id are required"})
+        const { name, description, user_id } = req.body;
+        const result = await createListLogic(name, description, user_id);
+        if (result.success) {
+            res.status(201).json({ message: "List created successfully", list: result.newList });
+        } else {
+            let statusCode = 400;
+            if (result.error === "The user does not exist") {
+                statusCode = 404;
+            }
+            return res.status(statusCode).json({ message: result.error });
         }
-        if (!validateDataTypes({name, description, user_id})) {
-            return res.status(400).json({ message: "Invalid data type"})
-        }
-        if(!await validateUserExists(user_id)){
-            return res.status(404).json({message: "The user does not exist"})
-        }
-        const newList = await ListModel.create({name: name, description: description, user_id: user_id})
-        res.status(201).json({ message: "List created successfully", list: newList})
     } catch (error) {
-        // Internal server error
         res.status(500).json({ message: error.message });
     }
-}
+};
 
 // obtain all list from a user
 export const getAllList = async (req, res) => {
     try {
-        let { user_id } = req.params
-        user_id = Number(user_id)
-        if(!user_id){
-            return res.status(400).json({message:"User is required"})
+        const { user_id } = req.params;
+        const result = await getAllListLogic(Number(user_id));
+        if (result.success) {
+            res.status(200).json(result.allLists);
+        } else {
+            let statusCode = 400;
+            if (result.error === "User not found") {
+                statusCode = 404;
+            }
+            res.status(statusCode).json({ message: result.error });
         }
-        if(!await validateUserExists(user_id)){
-            return res.status(404).json({ message: "User not found"})
-        }
-        const allLists = await ListModel.findAll({
-            where: { user_id: user_id}
-        })
-        res.status(200).json(allLists)
     } catch (error) {
-        // Internal server error
         res.status(500).json({ message: error.message });
     }
-}
+};
 
 // update an existing list
 export const updateList = async (req, res) => {
     try {
-        let {name, description} = req.body
-        let {id} = req.params
-        id = Number(id)
-        if(!id){
-            return res.status(400).json({message:"Id is required"})
+        const { name, description } = req.body;
+        const { id } = req.params;
+        const result = await updateListLogic(name, description, Number(id));
+        if (result.success) {
+            res.status(200).json({ message: "List updated successfully" });
+        } else {
+            let statusCode = 400;
+            if (result.error === "List not found") {
+                statusCode = 404;
+            }
+            res.status(statusCode).json({ message: result.error });
         }
-        if (!validateDataTypesUpdate({name, description, id})) {
-            return res.status(400).json({ message: "Invalid data type"})
-        }
-        // find list
-        const listToUpdate = await foundList(id)
-        if(!listToUpdate){
-            return res.status(404).json({message:"List not found"})
-        }
-        // update list
-        await ListModel.update({name, description}, {
-            where: {id: id}
-        })
-        res.status(200).json({message:"List updated successfully"})
     } catch (error) {
-        // Internal server error
         res.status(500).json({ message: error.message });
     }
-}
+};
 
 // deleting list
-export const deleteList = async(req, res) =>{
+export const deleteList = async (req, res) => {
     try {
-        let {id} = req.params
-        id = Number(id)
-        if(!id) {
-            return res.status(400).json({message:"Id is required"})
+        const { id } = req.params;
+        const result = await deleteListLogic(Number(id));
+        if (result.success) {
+            res.status(200).json({ message: "List deleted successfully" });
+        } else {
+            let statusCode = 400;
+            if (result.error === "List not found") {
+                statusCode = 404;
+            }
+            res.status(statusCode).json({ message: result.error });
         }
-        if(!validateDataTypes({id})){
-            return res.status(400).json({ message: "Invalid data type"})
-        }
-        const listToDelete = await foundList(id)
-        if(!listToDelete){
-            return res.status(404).json({message:"List not found"})
-        }
-        // delete relation
-        await ListGameModel.destroy({where:{list_id:id}})
-        listToDelete.user_id = null
-        
-        await ListModel.destroy({where:{id:id}})
-        res.status(200).json({message:"List deleted successfully"})
     } catch (error) {
-        // Internal server error
         res.status(500).json({ message: error.message });
     }
-}
-
-//----------------------------------------------------------------------
-// Modular functions
-//----------------------------------------------------------------------
-const validateDataTypes = (fields) => {
-    const dataTypes = {
-        name: "string",
-        user_id: "number",
-        id: "number",
-        description: ["string", "undefined"]
-    };
-    for (let key in fields) {
-        if (!dataTypes[key] || (typeof fields[key] !== dataTypes[key] && !dataTypes[key].includes(typeof fields[key]))) {
-            return false;
-        }
-    }
-    return true;
 };
-
-const validateDataTypesUpdate = (fields) => {
-    const dataTypesUpdate = {
-        name: ["string", "undefined"],
-        user_id: "number",
-        id: "number",
-        description: ["string", "undefined"]
-    };
-    for (let key in fields) {
-        if (!dataTypesUpdate[key] || (typeof fields[key] !== dataTypesUpdate[key] && !dataTypesUpdate[key].includes(typeof fields[key]))) {
-            return false;
-        }
-    }
-    return true;
-};
-
-const validateUserExists = async (user_id) => {
-    const user = await UserModel.findByPk(user_id);
-    return !!user; 
-}
-
-const foundList = async (list_id) => {
-    const list = await ListModel.findByPk(list_id)
-    return list
-}
---
+++ node/logic/ListLogic.js
@@ -0,0 +1,127 @@
+import ListModel from '../models/ListModel.js';
+import UserModel from "../models/UserModel.js";
+import ListGameModel from "../models/ListGameModel.js";
+
+//----------------------------------------------------------------------
+// CRUD Methods
+//----------------------------------------------------------------------
+
+// create a list
+export async function createListLogic(name, description, user_id) {
+    try {
+        if (!name || !user_id) {
+            throw new Error("Name and user_id are required");
+        }
+        if (!validateDataTypes({ name, description, user_id })) {
+            throw new Error("Invalid data type");
+        }
+        if (!await validateUserExists(user_id)) {
+            throw new Error("The user does not exist");
+        }
+        const newList = await ListModel.create({ name:name, description:description, user_id:user_id });
+        return { success: true, newList };
+    } catch (error) {
+        return { success: false, error: error.message };
+    }
+}
+
+// obtain all list from a user
+export async function getAllListLogic(user_id) {
+    try {
+        if (!user_id) {
+            throw new Error("User is required");
+        }
+        if (!await validateUserExists(user_id)) {
+            throw new Error("User not found");
+        }
+        const allLists = await ListModel.findAll({ where: { user_id } });
+        return { success: true, allLists };
+    } catch (error) {
+        return { success: false, error: error.message };
+    }
+}
+
+// update an existing list
+export async function updateListLogic(name, description, id) {
+    try {
+        if (!id) {
+            throw new Error("Id is required");
+        }
+        if (!validateDataTypesUpdate({ name, description, id })) {
+            throw new Error("Invalid data type");
+        }
+        const listToUpdate = await foundList(id);
+        if (!listToUpdate) {
+            throw new Error("List not found");
+        }
+        await ListModel.update({ name, description }, { where: { id } });
+        return { success: true };
+    } catch (error) {
+        return { success: false, error: error.message };
+    }
+}
+
+// deleting list
+export async function deleteListLogic(id) {
+    try {
+        if (!id) {
+            throw new Error("Id is required");
+        }
+        if (!validateDataTypes({ id })) {
+            throw new Error("Invalid data type");
+        }
+        const listToDelete = await foundList(id);
+        if (!listToDelete) {
+            throw new Error("List not found");
+        }
+        await ListGameModel.destroy({ where: { list_id: id } });
+        listToDelete.user_id = null;
+        await ListModel.destroy({ where: { id } });
+        return { success: true };
+    } catch (error) {
+        return { success: false, error: error.message };
+    }
+}
+
+//----------------------------------------------------------------------
+// Modular functions
+//----------------------------------------------------------------------
+const validateDataTypes = (fields) => {
+    const dataTypes = {
+        name: "string",
+        user_id: "number",
+        id: "number",
+        description: ["string", "undefined"]
+    };
+    for (let key in fields) {
+        if (!dataTypes[key] || (typeof fields[key] !== dataTypes[key] && !dataTypes[key].includes(typeof fields[key]))) {
+            return false;
+        }
+    }
+    return true;
+};
+
+const validateDataTypesUpdate = (fields) => {
+    const dataTypesUpdate = {
+        name: ["string", "undefined"],
+        user_id: "number",
+        id: "number",
+        description: ["string", "undefined"]
+    };
+    for (let key in fields) {
+        if (!dataTypesUpdate[key] || (typeof fields[key] !== dataTypesUpdate[key] && !dataTypesUpdate[key].includes(typeof fields[key]))) {
+            return false;
+        }
+    }
+    return true;
+};
+
+const validateUserExists = async (user_id) => {
+    const user = await UserModel.findByPk(user_id);
+    return !!user; 
+}
+
+const foundList = async (list_id) => {
+    const list = await ListModel.findByPk(list_id)
+    return list
+}
