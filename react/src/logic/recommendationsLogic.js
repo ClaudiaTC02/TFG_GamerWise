b787bbe0f5431fef1696a1c7d78106a1bacaba44
Claudia <claudiatcruz2002@gmail.com>
2024-05-26 16:52:03 +0200

    visualize games recommender in the lists

--- react/src/logic/recommendationsLogic.js
+++ react/src/logic/recommendationsLogic.js
@@ -1,5 +1,5 @@
 import { getGameDetailsRequest } from "../api/igdb";
-import { getLandingRecommendations } from "../api/recommendations";
+import { getLandingRecommendations, getListRecommendations } from "../api/recommendations";
 
 export const getLandingRecommendationsLogic = async (token) => {
   try {
@@ -16,3 +16,22 @@ export const getLandingRecommendationsLogic = async (token) => {
     throw new Error(error.response.data.message);
   }
 };
+
+export const getListRecommendationsLogic = async (token, list_id) => {
+  try {
+    const recommendations = await getListRecommendations(token, list_id);
+    const mappedGameData = recommendations.map(game => ({
+      game: {
+          id: game.igdb_id,
+          name: game.name,
+          cover: { url: game.cover },
+          first_release_date: game.release_date,
+          genres: game.gender.split(',').map(genre => ({ name: genre.trim() })),
+          platforms: game.platforms.split(',').map(platform => ({ name: platform.trim() })),
+      }
+  }));
+  return mappedGameData
+  } catch (error) {
+    throw new Error(error.response.data.message);
+  }
+}
--- react/src/pages/ListPage.jsx
+++ react/src/pages/ListPage.jsx
@@ -8,15 +8,16 @@ import { useEffect, useState } from "react";
 import { getGamesOfListWithRatingLogic } from "../logic/listLogic";
 import { ModalDeleteList, ModalUpdateList } from "../components/ModalLists";
 import { CarouselSection } from "../components/CarouselSection";
-import datamock from "../mock/latestGame.json";
 import { Loading } from "../components/Loading";
 import { Footer } from "../components/Footer";
+import { getListRecommendationsLogic } from "../logic/recommendationsLogic";
 
 export function ListPage() {
   const { id } = useParams();
   const { token } = useAuth();
   const [list, setList] = useState([]);
   const [games, setGames] = useState([]);
+  const [recommendations, setRecommendations] = useState([]);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   const [showList, setShowList] = useState(false);
@@ -31,6 +32,9 @@ export function ListPage() {
     const fetchListDetails = async () => {
       try {
         const list_result = await getGamesOfListWithRatingLogic(id, token);
+        const recommendations = await getListRecommendationsLogic(token, id)
+        console.log(recommendations)
+        setRecommendations(recommendations)
         console.log(list_result);
         setLoading(false);
         if (list_result) {
@@ -146,7 +150,7 @@ export function ListPage() {
         list={list}
       />
       <CarouselSection
-        gamesData={datamock}
+        gamesData={recommendations}
         text="Creemos que podrías incluir"
       />
       <Footer />
